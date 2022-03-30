091d482247254b8b2056d178549494167af25b5a
MatheusBaganha <matheusu.baganha@gmail.com>
2022-03-30 17:01:47 -0300

    animação no trocar-pizza

--- modules/trocar-pizza.js
+++ modules/trocar-pizza.js
@@ -4,23 +4,43 @@ export default function initTrocarPizza() {
     const linha = document.querySelectorAll('[data-linha]');
     const texto = document.querySelector('[data-texto]');
     const imagem = document.querySelector('[data-imagem]');
+    const animar = document.querySelector('[data-animar]');
 
+
+    //Vai trocar a cor da linha dos dois titulos 
     function trocaCor(cor) {
         linha.forEach((linha) => {
             linha.style.backgroundColor = cor;
         })
     }
 
+
+    //Muda a imagem da pizza para a selecionada
     function trocarImg(imagemLocal) {
         imagem.setAttribute('src', imagemLocal);
     }
 
+
+    //Ativa a animação do css e verifica caso ja tenha a classe para nao ter erros na hora de animar, como por ex, clicar uma vez, e na segunda vez pela classe ja estar ativada, não ativar mais a animação. O setTimeout tambem impede erros, serve pra nao deixar tudo instantaneo e travar a animação.
+    function animarConteudo() {
+        if (animar.classList.contains('animar')) {
+            animar.classList.remove('animar');
+            setTimeout(() => {
+                animar.classList.add('animar');
+            }, 10);
+        } else {
+            animar.classList.add('animar');
+        }
+    }
+
+
+    //Troca todo o conteudo sobre as pizzas de acordo com o circulo que foi clicado
     function trocarConteudo(conteudo) {
         if(conteudo.currentTarget === circulos[1]) {
             tituloPizza.innerText = 'Pizza Marguerita';
             texto.innerText = 'Sabor ícone da Itália que leva apenas molho, muçarela, tomate e manjericão. É uma ótima opção para quem não come carne.'
             trocaCor('white');
-            trocarImg('img/pizza-margherita.png')
+            trocarImg('img/cheese-pizza2.png')
 
         } else if(conteudo.currentTarget === circulos[2]) {
             tituloPizza.innerText = 'Pizza Portuguesa';
@@ -39,10 +59,10 @@ export default function initTrocarPizza() {
 
     }
 
-
+    //Eventos das funções
     circulos.forEach((circulo) => {
         circulo.addEventListener('click', trocarConteudo);
-        circulo.addEventListener('touchstart', trocarConteudo);
+        circulo.addEventListener('click', animarConteudo);
     })
 
 }

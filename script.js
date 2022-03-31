07cc9ebebb6188023a3456ebf18642e994d52cc6
MatheusBaganha <matheusu.baganha@gmail.com>
2022-03-31 18:15:27 -0300

    formulario para pedir pizza
    A interação já ta pronta, só precisa arrumar o css.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import initMenuMobile from './modules/menu-mobile.js';
 import initTrocarPizza from './modules/trocar-pizza.js';
+import initPedirPizza from './modules/pedir-pizza.js';
 
 
 initMenuMobile();
 initTrocarPizza();
+initPedirPizza();

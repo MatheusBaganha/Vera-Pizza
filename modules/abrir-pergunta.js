4b40cea22d24bd1375ebe47572b12969d3bc32c5
MatheusBaganha <matheusu.baganha@gmail.com>
2022-04-08 15:26:49 -0300

    posicionamentos de duvidas melhorado

--- modules/abrir-pergunta.js
+++ modules/abrir-pergunta.js
@@ -4,15 +4,16 @@ const containers = document.querySelectorAll('[data-container-pergunta]');
 const perguntas = document.querySelectorAll('[data-pergunta]');
 const respostas = document.querySelectorAll('[data-resposta]');
 
-function abrirPergunta(container) {
-    if(container.target) {
-        container.target.nextElementSibling.classList.toggle('abrir');
+function abrirPergunta(pergunta) {
+    if(pergunta.target) {
+        pergunta.target.nextElementSibling.classList.toggle('abrir');
     }
+
 }
 
 
-containers.forEach((container) => {
-    container.addEventListener('click', abrirPergunta);
+perguntas.forEach((pergunta) => {
+    pergunta.addEventListener('click', abrirPergunta);
 });
 
 }

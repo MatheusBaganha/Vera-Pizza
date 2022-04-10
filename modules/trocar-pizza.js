791a8543b4940a978988ac9fafcd4baf6185d591
MatheusBaganha <matheusu.baganha@gmail.com>
2022-04-10 17:38:06 -0300

    Cardápio finalizado

--- modules/trocar-pizza.js
+++ modules/trocar-pizza.js
@@ -43,7 +43,7 @@ export default function initTrocarPizza() {
 
     function trocarConteudo(conteudo) {
         if(conteudo.currentTarget === circulos[1]) {
-            tituloPizza.innerText = 'Pizza Marguerita';
+            tituloPizza.innerText = 'Pizza Margherita';
             texto.innerText = 'Sabor ícone da Itália que leva apenas molho, muçarela, tomate e manjericão. É uma ótima opção para quem não come carne.'
             trocaCor('white');
             trocarImg(imagem, 'img/cheese-pizza2.png');

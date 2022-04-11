1eabc1038a191f00d056df08393a67005ea84303
MatheusBaganha <matheusu.baganha@gmail.com>
2022-04-11 16:50:32 -0300

    animação nos itens do cardápio

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ import initMenuMobile from './modules/menu-mobile.js';
 import initTrocarPizza from './modules/trocar-pizza.js';
 import initPedirPizza from './modules/pedir-pizza.js';
 import initAbrirPergunta from './modules/abrir-pergunta.js';
+import initAnimaCardapio from './modules/anima-cardapio.js';
 
 initMenuMobile();
 initTrocarPizza();
 initPedirPizza();
 initAbrirPergunta();
+initAnimaCardapio();
